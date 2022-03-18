4f4f053f4e336e5cb3e7d505ea321ed35db228cf
hashbert <obert.chen@gmail.com>
2022-03-18 11:19:29 -0400

    disabled material on photo trigger so it doesn't block stuff in next scenes/menus

--- Assets/Scripts/LevelManager/Level/TriggerFader.cs
+++ Assets/Scripts/LevelManager/Level/TriggerFader.cs
@@ -6,8 +6,15 @@ using UnityEngine;
 public class TriggerFader : MonoBehaviour
 {
     [SerializeField] private GameObject fader;
+    [SerializeField] private Material plainMaterial;
+    [SerializeField] private GameObject kidObject;
+    [SerializeField] private GameObject adultObject;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         fader.SetActive(true);
+        adultObject.GetComponent<SpriteRenderer>().material = plainMaterial;
+        kidObject.GetComponent<SpriteRenderer>().material = plainMaterial;
+        GetComponent<SpriteRenderer>().material = plainMaterial;
     }
 }
