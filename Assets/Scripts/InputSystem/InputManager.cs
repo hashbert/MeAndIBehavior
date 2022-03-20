ef0efe6288c2164f8b940c0da47db2ff6da5e2af
Jeffrey Persons <jperman8@gmail.com>
2022-03-20 13:19:19 -0400

    Scene Management: Rename template scene from Level99 to LevelTemplate, and add warnings if attempting to run that scene

--- Assets/Scripts/InputSystem/InputManager.cs
+++ Assets/Scripts/InputSystem/InputManager.cs
@@ -1,9 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
-using System;
+
 
 public class InputManager : MonoBehaviour
 {
@@ -11,32 +9,56 @@ public class InputManager : MonoBehaviour
     public static PlayerInput playerInput;
 
     //saving level Number to player prefs
-    private int levelNumber;
+    private int? levelNumber;
     //getting Scene name for restarting
     private string sceneName;
+
+    public void OnRestartInput(InputAction.CallbackContext _)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 
     private void Awake()
     {
         sceneName = SceneManager.GetActiveScene().name;
         playerInput = GetComponent<PlayerInput>();
-        levelNumber = Int32.Parse(sceneName.Substring(sceneName.Length - 2, 2));
-        var highestLevel = PlayerPrefs.GetInt("Level");
-        levelNumber = levelNumber < highestLevel ? levelNumber = highestLevel : levelNumber;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        PlayerPrefs.SetInt("Level", levelNumber);
+
+        levelNumber = TryToExtractLevelFromSceneName(sceneName);
+        if (levelNumber.HasValue)
+        {
+            var highestLevel = PlayerPrefs.GetInt("Level");
+            levelNumber = levelNumber < highestLevel ? levelNumber = highestLevel : levelNumber;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        if (levelNumber.HasValue)
+        {
+            PlayerPrefs.SetInt("Level", levelNumber.Value);
+        }
+        else
+        {
+            Debug.LogWarning($"No level number found from scene name {sceneName} - nothing to save to player prefs");
+        }
     }
 
-    public void OnRestartInput(InputAction.CallbackContext context)
+
+    private static int? TryToExtractLevelFromSceneName(string sceneName, int suffixLength=2)
     {
-        SceneManager.LoadScene(sceneName);
+        Debug.Assert(suffixLength >= 1 && suffixLength < sceneName.Length,
+            "Suffix must be non empty and shorter than sceneName");
+
+        string sceneNameSuffix = sceneName.Substring(sceneName.Length - suffixLength);
+        if (int.TryParse(sceneNameSuffix, out int levelNumber))
+        {
+            return levelNumber;
+        }
+        else
+        {
+            Debug.LogWarning($"Expected scene name '{sceneName}' to end with {suffixLength} digits. " +
+                             $"If this is a template scene, try copying the scene and renaming to end with {suffixLength} digits");
+            return null;
+        }
     }
 }
