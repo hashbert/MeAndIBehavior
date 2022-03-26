0978b5c077b50a6464674914d999e64b486de706
hashbert <obert.chen@gmail.com>
2022-03-26 17:44:53 -0400

    added pause button to credits scene

--- Assets/Scripts/LevelManager/Credits/CreditsController.cs
+++ Assets/Scripts/LevelManager/Credits/CreditsController.cs
@@ -6,14 +6,45 @@ using UnityEngine.InputSystem;
 
 public class CreditsController : MonoBehaviour
 {
-    private readonly InputAction _anyKeyWait = new InputAction(binding: "/*/<button>", type: InputActionType.Button);
-    private void Awake() => _anyKeyWait.performed += DoSomething;
-    private void OnEnable() => _anyKeyWait.Enable();
-    private void OnDisable() => _anyKeyWait.Disable();
-    private void OnDestroy() => _anyKeyWait.performed -= DoSomething;
-    private void DoSomething(InputAction.CallbackContext ctx) => AnyKey();
-    private void AnyKey()
+    [SerializeField] private InputActionReference pause;
+    [SerializeField] private InputActionReference back;
+    [SerializeField] private Animator anim;
+
+    private void OnEnable() 
     {
-        SceneManager.LoadScene("MainMenuScene");
+        pause.action.Enable();
+        pause.action.performed += PauseCredits;
+        back.action.Enable();
+        back.action.started += BackToMainMenu;
+    }
+    private void OnDisable() 
+    {
+        pause.action.Disable();
+        pause.action.performed -= PauseCredits;
+        back.action.Disable();
+        back.action.started -= BackToMainMenu;
+    }
+
+    private void PauseCredits(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (anim.GetFloat("speed")!=0) 
+            {
+                anim.SetFloat("speed", 0);
+            }
+            else
+            {
+                anim.SetFloat("speed", 1);
+            }
+        }
+    }
+
+    private void BackToMainMenu(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            SceneManager.LoadScene("MainMenuScene");
+        }
     }
 }
