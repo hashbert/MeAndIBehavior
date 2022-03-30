afb975819318dd1e7286a8a985eed7a0dafc619c
hashbert <obert.chen@gmail.com>
2022-03-30 18:50:41 -0400

    adjusted all initial level cameras to correct size, minor tweaks to level size
    added trigger to level 12 if box falls over ledge, automatically restarts level.

---
+++ Assets/Scripts/LevelManager/Level/RestartLevel.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+    [SerializeField] private GameObject fader;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("triggered");
+        fader.GetComponent<Fader>().SetNextSceneName(SceneManager.GetActiveScene().name);
+        fader.SetActive(true);
+    }
+}
