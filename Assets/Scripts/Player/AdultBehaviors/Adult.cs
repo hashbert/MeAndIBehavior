f077cdf412ae6359e95165321b325752856f32f3
hashbert <obert.chen@gmail.com>
2022-06-24 09:11:53 -0400

    Update Adult.cs

--- Assets/Scripts/Player/AdultBehaviors/Adult.cs
+++ Assets/Scripts/Player/AdultBehaviors/Adult.cs
@@ -115,12 +115,12 @@ public class Adult : MonoBehaviour
     private IEnumerator SmokeAndTeleport()
     {
         _smoke.Play();
-        InputManager.playerInput.actions.Disable();
+        //InputManager.playerInput.actions.Disable();
         yield return new WaitForSeconds(.5f);
         StartCoroutine(_cinemachineBehavior.PlayKidCamera());
         yield return new WaitForSeconds(.75f);
         transform.position = Kid.transform.position + new Vector3(0, teleportHeight);
-        InputManager.playerInput.actions.Enable();
+        //InputManager.playerInput.actions.Enable();
 
     }
     #endregion
