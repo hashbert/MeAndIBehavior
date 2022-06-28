8e070381160948443a7a2e8b429e5e3ea71884ee
hashbert <obert.chen@gmail.com>
2022-06-28 11:29:46 -0400

    Update RestartLevel.cs

--- Assets/Scripts/LevelManager/Level/RestartLevel.cs
+++ Assets/Scripts/LevelManager/Level/RestartLevel.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class RestartLevel : MonoBehaviour
 {
     [SerializeField] private GameObject fader;
-
     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        RestartCurrentLevel();
+    }
+    public void RestartCurrentLevel()
     {
         fader.GetComponent<Fader>().SetNextSceneName(SceneManager.GetActiveScene().name);
         fader.SetActive(true);
